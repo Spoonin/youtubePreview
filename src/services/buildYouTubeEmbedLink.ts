4b1e6a7f1fe2088890e25b2ef147d81309ec3a93
Denis Lozhkin <dlozhkin@xbsoftware.com>
2020-12-18 22:32:44 +0300

    fix link builder

--- src/services/buildYouTubeEmbedLink.ts
+++ src/services/buildYouTubeEmbedLink.ts
@@ -2,5 +2,5 @@ import { YoutubeLinkModel } from "../models/YouTubeLinkModel"
 
 export const buildYouTubeEmbedLink = (youtubeData: YoutubeLinkModel) =>
     `https://www.youtube.com/embed/${youtubeData.resourceId}${
-        youtubeData.startAt && `?start=${youtubeData.startAt}`
+        (youtubeData.startAt && `?start=${youtubeData.startAt}`) || ""
     }`
