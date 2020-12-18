61226a6ce4ed58a6c99ab812e6475734f29cd527
Denis Lozhkin <dlozhkin@xbsoftware.com>
2020-12-18 17:07:19 +0300

    fix parser errors

--- src/services/parseYouTubeLink.ts
+++ src/services/parseYouTubeLink.ts
@@ -8,34 +8,39 @@ const START_PARAM_NAME = "t"
 export const parseYouTubeLink = (
     link: string
 ): YoutubeLinkModel | undefined => {
-    const url = new URL(link)
-    const host = url.host.replace("www.", "")
-    let resourceId
+    try {
+        const url = new URL(link)
 
-    // define resourceId
-    switch (host) {
-        case YOUTUBE_COM_HOST: {
-            resourceId = url.searchParams.get(ID_PARAM_NAME)
-            break
+        const host = url.host.replace("www.", "")
+        let resourceId
+
+        // define resourceId
+        switch (host) {
+            case YOUTUBE_COM_HOST: {
+                resourceId = url.searchParams.get(ID_PARAM_NAME)
+                break
+            }
+            case YOUTU_BE_HOST: {
+                resourceId = url.pathname.split("/")[1]
+                break
+            }
+            default:
+                return undefined // unknown host
         }
-        case YOUTU_BE_HOST: {
-            resourceId = url.pathname.split("/")[1]
-            break
+
+        const startString = url.searchParams.get(START_PARAM_NAME)
+
+        let startAt
+
+        if (startString && !Number.isNaN(+startString)) {
+            startAt = +startString
         }
-        default:
-            return undefined // unknown host
-    }
 
-    const startString = url.searchParams.get(START_PARAM_NAME)
-
-    let startAt
-
-    if (startString && !Number.isNaN(+startString)) {
-        startAt = +startString
-    }
-
-    if (resourceId) {
-        return { resourceId, startAt }
+        if (resourceId) {
+            return { resourceId, startAt }
+        }
+    } catch {
+        return undefined
     }
 
     return undefined
