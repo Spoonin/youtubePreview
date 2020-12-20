0e1f376e91248837d46155366852a32baadd400a
Denis Lozhkin <dlozhkin@xbsoftware.com>
2020-12-20 11:19:37 +0300

    link buikder fix

--- src/services/buildYouTubeEmbedLink.ts
+++ src/services/buildYouTubeEmbedLink.ts
@@ -1,6 +1,6 @@
-import { YoutubeLinkModel } from "../models/YouTubeLinkModel"
+import { YoutubeLink } from "../models/YouTubeLink"
 
-export const buildYouTubeEmbedLink = (youtubeData: YoutubeLinkModel) =>
-    `https://www.youtube.com/embed/${youtubeData.resourceId}${
+export const buildYouTubeEmbedLink = (youtubeData: YoutubeLink) =>
+    `https://www.youtube.com/embed/${youtubeData.videoId}${
         (youtubeData.startAt && `?start=${youtubeData.startAt}`) || ""
     }`
